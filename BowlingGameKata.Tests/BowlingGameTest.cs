e5e851c6e461733db99489b02179e0ee2342caaf
moorecj <moore.cyril@gmail.com>
2014-06-05 09:40:39 -0400

    Initial Commit

---
+++ BowlingGameKata.Tests/BowlingGameTest.cs
@@ -0,0 +1,114 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata.Tests
+{
+
+    [TestFixture]
+    class BowlingGameTest
+    {
+        private Game game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            game = new Game();
+        }
+
+
+        [Test]
+        public void GameClass_ShouldExist()
+        {
+            Game game = new Game();
+            Assert.That(game, Is.Not.Null);
+
+        }
+
+        [Test]
+        public void RollingAllZerosShouldScoreZero()
+        {
+
+            RollMany(0, 20);
+
+            Assert.That(game.Score(), Is.EqualTo(0));
+         
+
+        }
+
+        private void RollMany( int pinValue, int numberOFRolls )
+        {
+            for (var i = 0; i < numberOFRolls; ++i)
+            {
+                game.Roll( pinValue );
+            }
+        }
+
+        [Test]
+        public void RollingAllOnesShouldScoreTwenty()
+        {
+
+            RollMany(1, 20);
+
+            Assert.That(game.Score(), Is.EqualTo(20));
+
+
+        }
+
+        [Test]
+        public void RollingAllTwosShouldScoreForty()
+        {
+
+            RollMany(2,20);
+
+            Assert.That(game.Score(), Is.EqualTo(40));
+
+
+        }
+
+        [Test]
+        public void RollingSomeSparesShouldAddNextRollToScore()
+        {
+            game.Roll(7);
+            game.Roll(3);
+            game.Roll(2);
+
+            Assert.That(game.Score(), Is.EqualTo(14));
+        }
+
+        [Test]
+        public void RollingAStrikeShouldAddNextTwoRollsToScore()
+        {
+            game.Roll(10);
+            game.Roll(3);
+            game.Roll(5);
+
+            Assert.That(game.Score(), Is.EqualTo(26));
+        }
+
+        [Test]
+        public void RollingATurkeyshouldScoreSixty()
+        {
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+
+            Assert.That(game.Score(), Is.EqualTo(60));
+        }
+
+        [Test]
+        public void RollingAllStrikesShouldResultInThreeHundred()
+        {
+            RollMany(10, 12);
+
+            Assert.That(game.Score(), Is.EqualTo(300));
+
+        }
+
+
+    }
+
+}
---
+++ BowlingGameKata/Game.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata
+{
+    public class Game
+    {
+
+        private int[] rolls;
+        private int rollCounter;
+
+        public Game()
+        {
+
+           rolls = new int[20];
+           rollCounter = 0; 
+        }
+
+        public void Roll(int pins)
+        {
+            rolls[rollCounter] += pins;
+            rollCounter++;
+        }
+
+        public int Score()
+        {
+            var score = 0;
+            var rollFrameIndex = 0;
+
+            for (var frame = 0; frame < 10; frame = frame +1 )
+            {
+
+                if (IsStrike(rollFrameIndex))
+                {
+                    score += ( 10 + rolls[rollFrameIndex + 1] + rolls[rollFrameIndex + 2] );
+                    rollFrameIndex = rollFrameIndex + 1;
+                }
+                else if (IsSpare(rollFrameIndex))
+                {
+                    score += 10 + rolls[rollFrameIndex + 2];
+                    rollFrameIndex = rollFrameIndex + 2;
+                }
+                else
+                {
+                    score += rolls[rollFrameIndex] + rolls[rollFrameIndex + 1];
+                    rollFrameIndex = rollFrameIndex + 2; 
+                }
+
+            }
+
+        return  score;
+
+        }
+
+        private bool IsSpare(int rollFrameIndex)
+        {
+            return rolls[rollFrameIndex] + rolls[rollFrameIndex + 1] == 10;
+        }
+
+        private bool IsStrike(int rollFrameIndex)
+        {
+            return rolls[rollFrameIndex] == 10;
+        }
+
+    }
+}
